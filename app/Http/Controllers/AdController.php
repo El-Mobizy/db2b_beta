c6a656f3811500fad5a58df8d90a938a90b4af4d
ReloDev <ayenaaurel15@gmail.com>
2024-07-02 11:12:45 +0100

    commit

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -910,10 +910,9 @@ public function checkCategoryShop($ownerId, Request $request){
 
     private function createAd(Request $request){
 
-        $commission = 1.15;
         $title = htmlspecialchars($request->input('title'));
         $price = htmlspecialchars($request->input('price'));
-        $final_price = floatval($request->input('price'))*$commission;
+        $final_price = floatval($request->input('price'));
         $location_id = htmlspecialchars($request->input('location_id'));
         $category_id = htmlspecialchars($request->input('category_id'));
         $shop_id = htmlspecialchars($request->input('shop_id'));
